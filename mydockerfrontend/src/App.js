5c63f27f76c68b40e793155bf6f97dbb5cce781b
dkwitt <56313807+dkwitt@users.noreply.github.com>
2020-04-02 17:28:19 +0200

    Update App.js

--- mydockerfrontend/src/App.js
+++ mydockerfrontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World, hello CI!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React edit
+          Learn React
         </a>
       </header>
     </div>
